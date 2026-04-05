1017d1000acdbedee68bdd930241b769069d5840
yuju <yuju99924@gmail.com>
2026-04-05 17:18:05 +0800

    feat: 新增登出功能

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,6 +1,24 @@
-import { Link, Outlet } from "react-router";
+import axios from "axios";
+import { Link, Outlet, useNavigate } from "react-router";
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function AdminLayout() {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await axios.post(`${API_BASE}/logout`);
+      //清除cookie
+      document.cookie =
+        "auth_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      alert("已登出");
+    } catch {
+      alert("登出失敗");
+    } finally {
+      navigate("/", { replace: true });
+    }
+  };
   return (
     <>
       <header>
@@ -15,6 +33,11 @@ function AdminLayout() {
               <Link className="nav-link" to="/admin/orders">
                 後台訂單列表
               </Link>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link" to="/" onClick={handleLogout}>
+                登出
+              </button>
             </li>
           </ul>
         </div>
